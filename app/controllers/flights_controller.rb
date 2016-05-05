9374c6a247e9e8fcd3a7779a2016af4897d187d3
Fraz Ahsan <fraz.ahsan@phaedrasolutions.com>
2016-05-06 02:07:39 +0500

    FLights view added

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,10 +4,58 @@ class FlightsController < ApplicationController
     response = Scanner.live_price(params[:flight])
     cookies[:prices_url] = response[:prices_url]
     @prices = HTTParty.get(cookies[:prices_url])
+    if @prices["Legs"].present?
+      set_hash
+    end
   end
 
   def refresh
     @prices = HTTParty.get(cookies[:prices_url])
-    render template: "flights/live_prices"
+    # $redis.set("price", @prices.to_json) 
+    if @prices["Legs"].present?
+      set_hash
+    end
+    respond_to do |format|
+      format.js 
+    end
+  end
+
+  private
+  def set_hash
+    @cheap = @prices["Legs"]
+    agents = @prices["Agents"]
+    places = @prices["Places"]
+    segments = @prices["Segments"]
+    itineraries = @prices["Itineraries"]
+    carriers = @prices["Carriers"]
+    currencies = @prices["Currencies"]
+
+    @cheap.each_with_index do |(k, v), index|
+      @cheap[index]["carrierDetail"] = []
+      @cheap[index]["Carriers"].each_with_index do |c, cc|
+        @cheap[index]["carrierDetail"][cc] = carriers.find { |h| h['Id'] == c }
+      end
+      @cheap[index]["Operators"] = []
+      @cheap[index]["OperatingCarriers"].each_with_index do |c, cc|
+        @cheap[index]["Operators"][cc] = carriers.find { |h| h['Id'] == c }
+      end
+      @cheap[index]["Segments"] = []
+      @cheap[index]["SegmentIds"].each_with_index do |c, cc|
+        @cheap[index]["Segments"][cc] = segments.find { |h| h['Id'] == c }
+      end
+      @cheap[index]["Destination"] = places.find { |h| h['Id'] == @cheap[index]["DestinationStation"] }
+      @cheap[index]["Origin"] = places.find { |h| h['Id'] == @cheap[index]["OriginStation"] } 
+      @cheap[index]["PriceInfo"] = itineraries.find { |h| h['OutboundLegId'] == @cheap[index]["Id"] }["PricingOptions"]
+      price = 0
+      @cheap[index]["PriceInfo"].each do |p|
+        price += p["Price"] 
+      end
+      @cheap[index]["TotalPrice"] = price.round(2)
+      @cheap[index]["CurrencyCode"] = currencies.find { |h| h['Code'] == cookies[:currency]}["Symbol"] 
+    end
+    @cheap = @cheap.sort_by { |k| k["TotalPrice"]}
+
+    @duration  = @cheap.deep_dup
+    @duration = @duration.sort_by { |k| k["Duration"]} 
   end
 end
