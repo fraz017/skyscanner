7915056255d97b7b0dba36a84a441417b45795ef
Fraz Ahsan <fraz.ahsan17@gmail.com>
2016-05-23 22:18:36 -0700

    Name removed from db

--- db/migrate/20160521141439_create_user_notifications.rb
+++ db/migrate/20160521141439_create_user_notifications.rb
@@ -1,7 +1,6 @@
 class CreateUserNotifications < ActiveRecord::Migration
   def change
     create_table :user_notifications do |t|
-      t.string :name
       t.string :email
       t.float :price
       t.text :query
