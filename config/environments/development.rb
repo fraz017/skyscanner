5f9c097d234ecba189ac78dc59c50b18c0bed3aa
Fraz Ahsan <fraz.ahsan17@gmail.com>
2016-07-20 22:16:58 +0500

    Letter opener added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,15 +38,17 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.delivery_method = :letter_opener
 
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :user_name            => "skyfare4@gmail.com",
-   :password             => "imgreat1",
-   :authentication       => "plain",
-  :enable_starttls_auto => true
-  }
+  # config.action_mailer.smtp_settings = {
+  #  :address              => "smtp.gmail.com",
+  #  :port                 => 587,
+  #  :user_name            => "skyfare4@gmail.com",
+  #  :password             => "imgreat1",
+  #  :authentication       => "plain",
+  # :enable_starttls_auto => true
+  # }
+
 end
